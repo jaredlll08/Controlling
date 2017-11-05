af0e1e013431adbdddecf06994c6f0c6825d7aeb
Jared <jaredlll08@gmail.com>
2017-11-05 18:19:30 +0200

    version push

--- src/main/java/us/getfluxed/controlsearch/reference/Reference.java
+++ src/main/java/us/getfluxed/controlsearch/reference/Reference.java
@@ -7,7 +7,7 @@ public class Reference {
 
     public static final String MODID = "controlling";
     public static final String NAME = "Controlling";
-    public static final String VERSION = "3.0.3";
+    public static final String VERSION = "3.0.4";
 
 
 }

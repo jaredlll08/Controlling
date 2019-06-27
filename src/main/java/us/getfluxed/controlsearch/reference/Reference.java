c585192da9029d3e651f3d8524905adfbf1c1b28
Jared <jaredlll08@gmail.com>
2019-06-27 11:18:26 +0200

    Update Reference.java

--- src/main/java/us/getfluxed/controlsearch/reference/Reference.java
+++ src/main/java/us/getfluxed/controlsearch/reference/Reference.java
@@ -7,7 +7,7 @@ public class Reference {
 
     public static final String MODID = "controlling";
     public static final String NAME = "Controlling";
-    public static final String VERSION = "2.0.5";
+    public static final String VERSION = "2.0.6";
 
 
 }

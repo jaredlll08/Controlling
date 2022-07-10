42013ad3c316dfd9fe95439addcdeab7b4ec47de
Jared <jaredlll08@gmail.com>
2022-07-10 23:15:56 +0200

    port to newer forge Close #123
    thanks breaking changes

--- Forge/src/main/java/com/blamejared/controlling/events/ClientEventHandler.java
+++ Forge/src/main/java/com/blamejared/controlling/events/ClientEventHandler.java
@@ -4,17 +4,17 @@ import com.blamejared.controlling.client.NewKeyBindsScreen;
 import com.blamejared.controlling.mixin.AccessOptionsSubScreen;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.controls.KeyBindsScreen;
-import net.minecraftforge.client.event.ScreenOpenEvent;
+import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class ClientEventHandler {
     
     @SubscribeEvent
-    public void openGui(ScreenOpenEvent event) {
+    public void openGui(ScreenEvent.Opening event) {
         
         try {
             if(event.getScreen() instanceof KeyBindsScreen gui && !(event.getScreen() instanceof NewKeyBindsScreen)) {
-                event.setScreen(new NewKeyBindsScreen(((AccessOptionsSubScreen) gui).controlling$getLastScreen(), Minecraft.getInstance().options));
+                event.setNewScreen(new NewKeyBindsScreen(((AccessOptionsSubScreen) gui).controlling$getLastScreen(), Minecraft.getInstance().options));
             }
         } catch(Exception e) {
             e.printStackTrace();

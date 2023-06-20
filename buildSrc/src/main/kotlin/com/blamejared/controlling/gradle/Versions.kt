75bf5d3a8a1e38c1e7eb83d23308af26eb9e8c83
Jared <jaredlll08@gmail.com>
2023-06-20 16:32:02 +0200

    Port to 1.20.1
    Close #151

--- buildSrc/src/main/kotlin/com/blamejared/controlling/gradle/Versions.kt
+++ buildSrc/src/main/kotlin/com/blamejared/controlling/gradle/Versions.kt
@@ -3,9 +3,9 @@ package com.blamejared.controlling.gradle
 object Versions {
     const val MOD = "12.0"
     const val JAVA = "17"
-    const val MINECRAFT = "1.20"
-    const val FORGE = "46.0.14"
+    const val MINECRAFT = "1.20.1"
+    const val FORGE = "47.0.4"
     const val FABRIC_LOADER = "0.14.21"
-    const val FABRIC = "0.83.0+1.20"
-    const val SEARCHABLES = "1.0.2"
+    const val FABRIC = "0.83.1+1.20.1"
+    const val SEARCHABLES = "1.0.1"
 }

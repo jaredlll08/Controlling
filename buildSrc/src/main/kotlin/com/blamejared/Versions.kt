53dec48ef235e8b75961415f34932ed8db3c31f8
Jared <jaredlll08@gmail.com>
2025-07-04 02:23:54 -0600

    Port to 1.21.7

--- buildSrc/src/main/kotlin/com/blamejared/Versions.kt
+++ buildSrc/src/main/kotlin/com/blamejared/Versions.kt
@@ -1,14 +1,14 @@
 package com.blamejared
 
 object Versions {
-    const val MOD = "24.0"
+    const val MOD = "25.0"
     const val JAVA = "21"
-    const val MINECRAFT = "1.21.6"
+    const val MINECRAFT = "1.21.7"
     const val FABRIC_LOADER = "0.16.14"
-    const val FABRIC = "0.128.1+1.21.6"
-    const val FORGE = "56.0.9"
-    const val FORGE_LOADER = "[56,)"
-    const val NEO_FORGE = "21.6.20-beta"
+    const val FABRIC = "0.128.2+1.21.7"
+    const val FORGE = "57.0.2"
+    const val FORGE_LOADER = "[57,)"
+    const val NEO_FORGE = "21.7.3-beta"
     const val NEO_FORGE_LOADER= "[4,)"
-    const val SEARCHABLES = "1.0.2"
+    const val SEARCHABLES = "1.0.1"
 }
--- neoforge/build.gradle.kts
+++ neoforge/build.gradle.kts
@@ -6,7 +6,7 @@ import net.darkhax.curseforgegradle.TaskPublishCurseForge
 
 plugins {
     id("blamejared-modloader-conventions")
-    id("net.neoforged.moddev") version ("2.0.46-beta")
+    id("net.neoforged.moddev") version ("2.0.97")
     id("com.modrinth.minotaur")
 }
 

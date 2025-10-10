e885ba4d1a57f2c30dee2f01a3d1df8504a56fa5
Jared <jaredlll08@gmail.com>
2025-10-10 17:14:33 -0600

    Port to 1.21.10

--- buildSrc/src/main/kotlin/com/blamejared/Versions.kt
+++ buildSrc/src/main/kotlin/com/blamejared/Versions.kt
@@ -1,14 +1,14 @@
 package com.blamejared
 
 object Versions {
-    const val MOD = "27.0"
+    const val MOD = "28.0"
     const val JAVA = "21"
-    const val MINECRAFT = "1.21.9"
-    const val FABRIC_LOADER = "0.17.2"
-    const val FABRIC = "0.134.0+1.21.9"
-    const val FORGE = "59.0.1"
-    const val FORGE_LOADER = "[59,)"
-    const val NEO_FORGE = "21.9.11-beta"
+    const val MINECRAFT = "1.21.10"
+    const val FABRIC_LOADER = "0.17.3"
+    const val FABRIC = "0.134.1+1.21.10"
+    const val FORGE = "60.0.3"
+    const val FORGE_LOADER = "[60,)"
+    const val NEO_FORGE = "21.10.1-beta"
     const val NEO_FORGE_LOADER= "[4,)"
-    const val SEARCHABLES = "1.0.2"
+    const val SEARCHABLES = "1.0.1"
 }
